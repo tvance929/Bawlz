8de38848eb6e42817c4bc94b7a1397f8d7c1bdc8
Todd Vance <toddvance@tv-Computer.local>
2012-03-01 18:47:00 -0600

    Initial Commit

---
+++ Bawlz/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Bawlz
+//
+//  Created by Todd Vance on 3/1/12.
+//  Copyright Digital Fuel 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
